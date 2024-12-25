2cd02fd511510634b3c68d14ad60075c9d166528
dhruvbriotechno <154588490+dhruvbriotechno@users.noreply.github.com>
2024-12-25 17:59:25 +0530

    first commit

---
+++ src/app/home/page.jsx
@@ -0,0 +1,12 @@
+'use client';
+
+
+const page = () => {
+  return (
+    <div>
+      Home page
+    </div>
+  )
+}
+
+export default page
---
+++ src/app/page.jsx
@@ -0,0 +1,24 @@
+'use client';
+import Image from "next/image";
+import { useEffect, useState } from "react";
+
+export default function Home() {
+
+  // const [isLoaded, setLoaded] = useState(false)
+
+  // useEffect(() => {
+  //     setLoaded(true)
+  // }, [])
+  // if(!isLoaded){
+  //   return (
+  //     <div>
+  //       Loading...
+  //     </div>
+  //   )
+  // }
+  return (
+    <>
+    Dhruv
+    </>
+  );
+}
